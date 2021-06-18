a2218ca69256802125c3ce27161988eddd3ba6f1
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2021-06-18 14:49:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txtTmp;
+            int Z, quociente;
+
+            Console.Clear();
+            Console.Write("Informe um numero Z*+: ");
+            txtTmp = Console.ReadLine();
+
+            Z = int.Parse(txtTmp);
+            quociente = Z % 2;
+
+            Console.WriteLine("Resultado: " + quociente);
+        }
+    }
+}
